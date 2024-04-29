df8290959fadcddefcd11c8c3953f0506e0021b0
Arno Schuerhoff <arno.schuerhoff@havi.com>
2024-04-29 08:57:58 +0200

    add single characters to bulk header, restored Apache licenses

--- src/main/java/com/havi/example/ReproduceAMQ149005Backup.java
+++ src/main/java/com/havi/example/ReproduceAMQ149005Backup.java
@@ -1,12 +1,26 @@
 /*
- * Reproduce AMQ149005 in backup broker
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+ // Modified by Arno Schuerhoff 
+
 package com.havi.example;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
@@ -15,13 +29,6 @@ import javax.naming.InitialContext;
 
 import org.apache.activemq.artemis.util.ServerUtil;
 
-/**
- * Example of live and replicating backup pair.
- * <p>
- * After both servers are started, the live server is killed and the backup becomes active ("fails-over").
- * <p>
- * Later the live server is restarted and takes back its position by asking the backup to stop ("fail-back").
- */
 public class ReproduceAMQ149005Backup {
 
    private static Process server0;
@@ -29,7 +36,6 @@ public class ReproduceAMQ149005Backup {
    private static Process server1;
 
    public static void main(final String[] args) throws Exception {
-      final int numMessages = 30;
 
       Connection connection = null;
 
@@ -49,34 +55,27 @@ public class ReproduceAMQ149005Backup {
          // Step 3. Create a JMS Connection
          connection = connectionFactory.createConnection();
 
-         // Step 4. Create a *transacted* JMS Session with client acknowledgement
+         // Step 4. Create JMS Session with client acknowledgement
          Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
 
-         // Step 5. Start the connection to ensure delivery occurs
-         // connection.start();
-
-         // Step 6. Create a JMS MessageProducer and a MessageConsumer
+          // Step 5. Create a JMS MessageProducer and a MessageConsumer
          MessageProducer producer = session.createProducer(queue);
-         //MessageConsumer consumer = session.createConsumer(queue);
-
-         // Step 7. Send some messages to server #1, the live server
+  
+         // Step 6. Send  message with properties size near to 490K to server #1, the live server
          TextMessage message = session.createTextMessage("This is a text message.");
 
          StringBuilder strBuilder = new StringBuilder();
-         for (int i = 0; i < 15671; ++i) { 
-            strBuilder.append("xxxxxxxxxxxxxxxx");
+         for (int i = 0; i < 250736; ++i) { 
+            strBuilder.append('x');
          } 
          message.setStringProperty("bulk", strBuilder.toString());
 
          System.out.println("Sending message with bulk header length " + 2*strBuilder.length());
          producer.send(message);
          System.out.println("Message sent");
-        
-         
-
  
       } finally {
-         // Step 13. Be sure to close our resources!
+         // Step 7. Be sure to close our resources!
 
          if (connection != null) {
             connection.close();
